3eb769c8f4a182f7f929ad5286217890523dea8f
iHoHyeon <hyunee169@naver.com>
2022-03-07 16:37:02 +0900

    clone coding - canvas tutorial #waves

---
+++ Canvas-Tutorial/waves/README.md
@@ -0,0 +1,5 @@
+## [CLICK ME](https://ihohyeon.github.io/interactive-practice/Canvas-Tutorial/waves)
+
+### clone reference
+
+https://www.youtube.com/watch?v=LLfhY4eVwDY&list=PLGf_tBShGSDNGHhFBT4pKFRMpiBrZJXCm&index=7
---
+++ Canvas-Tutorial/waves/point.js
@@ -0,0 +1,15 @@
+export default class Point {
+  constructor(index, x, y) {
+    this.x = x;
+    this.y = y;
+    this.fixedY = y;
+    this.speed = 0.1 / 2;
+    this.cur = index;
+    this.max = Math.random() * 100 + 150;
+  }
+
+  update() {
+    this.cur += this.speed;
+    this.y = this.fixedY + Math.sin(this.cur) * this.max; // Maht.sin()을 이용해 [-1,1]을 진동하도록 한다.
+  }
+}
