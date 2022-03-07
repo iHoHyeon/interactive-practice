83981f664b28f688c464e94c628791d14deb1826
iHoHyeon <hyunee169@naver.com>
2022-03-07 17:45:51 +0900

    clone coding - canvas tutorial #rotating

---
+++ Canvas-Tutorial/rotating/README.md
@@ -0,0 +1,5 @@
+## [CLICK ME](https://ihohyeon.github.io/interactive-practice/Canvas-Tutorial/rotating)
+
+### clone reference
+
+https://www.youtube.com/watch?v=urDcoyIc6VQ&list=PLGf_tBShGSDNGHhFBT4pKFRMpiBrZJXCm&index=9
---
+++ Canvas-Tutorial/rotating/app.js
@@ -0,0 +1,64 @@
+import Polygon from './polygon.js';
+
+class App {
+  constructor() {
+    this.canvas = document.createElement('canvas');
+    this.ctx = this.canvas.getContext('2d');
+    document.body.appendChild(this.canvas);
+
+    window.addEventListener('resize', this.resize.bind(this), false);
+    this.resize();
+
+    this.isDown = false;
+    this.moveX = 0;
+    this.offSetX = 0;
+
+    document.addEventListener('pointerdown', this.onDown.bind(this), false);
+    document.addEventListener('pointermove', this.onMove.bind(this), false);
+    document.addEventListener('pointerup', this.onUp.bind(this), false);
+
+    requestAnimationFrame(this.animate.bind(this));
+  }
+
+  resize() {
+    this.stageWidth = document.body.clientWidth;
+    this.stageHeight = document.body.clientHeight;
+
+    this.canvas.width = this.stageWidth * 2;
+    this.canvas.height = this.stageHeight * 2;
+    this.ctx.scale(2, 2);
+
+    this.polygon = new Polygon(this.stageWidth / 2, this.stageHeight + this.stageHeight / 4, 500, 12);
+  }
+
+  animate() {
+    this.ctx.clearRect(0, 0, this.stageWidth, this.stageHeight);
+
+    this.moveX *= 0.92;
+
+    this.polygon.animate(this.ctx, this.moveX);
+
+    requestAnimationFrame(this.animate.bind(this));
+  }
+
+  onDown(e) {
+    this.isDown = true;
+    this.moveX = 0;
+    this.offSetX = e.clientX;
+  }
+
+  onMove(e) {
+    if (this.isDown) {
+      this.moveX = e.clientX - this.offSetX;
+      this.offSetX = e.clientX;
+    }
+  }
+
+  onUp(e) {
+    this.isDown = false;
+  }
+}
+
+window.onload = () => {
+  new App();
+};
